78d9c8d1a13e79b0e67c51b9d72dfde9560be431
Justin Yoo <justin.yoo@aliencube.com>
2015-09-02 11:07:20 +1000

    Initial source code commit

---
+++ src/TypeScriptAngularApp/App/components/mainContent/mainContent.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../Scripts/typings/angularjs/angular.d.ts" />
+
+"use strict";
+
+module app.angular.Directives {
+    export interface IMainContentScope extends ng.IScope {
+        username: string;
+        password: string;
+    }
+
+    export class MainContent implements ng.IDirective {
+        replace = true;
+        restrict = "EA";
+        scope = {};
+        templateUrl = "/App/components/mainContent/mainContent.html";
+
+        controller($scope: IMainContentScope) {
+            $scope.username = "Enter username";
+            $scope.password = "Enter password";
+        }
+    }
+}
+
+angular.module("app").directive("mainContent", () => new app.angular.Directives.MainContent());
+
