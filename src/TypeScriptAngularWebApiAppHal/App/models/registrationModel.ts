a969e4c6a9f6ecdaac53bc44923aa322a8b9f300
Justin Yoo <justin.yoo@aliencube.com>
2015-09-08 00:27:39 +1000

    Add AngularJS scripts to TypeScriptAngularWebApiAppHal project

---
+++ src/TypeScriptAngularWebApiAppHal/App/models/registrationModel.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../Scripts/typings/angularjs/angular.d.ts" />
+
+"use strict";
+
+module app.angular.Models {
+    export class Link {
+        href: string;
+        templated: boolean;
+    }
+
+    export class SelfLink {
+        self: Link;
+    }
+
+    export class Salutation {
+        text: string;
+        value: string;
+        _links: SelfLink;
+    }
+
+    export class RegistrationModel {
+        _embedded: Array<Salutation>;
+        _links: SelfLink;
+    }
+}
